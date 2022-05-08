7bef7cf722fb697212950e72932880d4672ad2c7
Jingnan Shi <jnshi@mit.edu>
2022-05-07 20:15:45 -0400

    tape implementation wip

--- src/reverse.rs
+++ src/reverse.rs
@@ -1,70 +1,67 @@
 use std::borrow::BorrowMut;
 use std::ops::{Add, Div, Mul, Neg, Sub, Index, AddAssign, MulAssign};
-use std::cell::RefCell;
+use std::cell::{Ref, RefCell};
 use std::fmt::{Debug, Formatter};
 use std::rc::Rc;
 
-// https://ricardomartins.cc/2016/06/08/interior-mutability
-// https://github.com/Rufflewind/revad/blob/eb3978b3ccdfa8189f3ff59d1ecee71f51c33fd7/revad.py
+/// Node in the computation graph
+#[derive(Clone, Copy)]
+pub struct Node {
+    /// partials of the two parents with respect to this node
+    pub partials: [f64; 2],
+    /// Parents to this node on the computation graph
+    pub parents: [usize; 2],
+}
 
-type VarRef = Rc<RefCell<Var>>;
+/// Tape holding the computation graph
+pub struct Tape {
+    pub vars: RefCell<Vec<Node>>,
+}
 
-#[derive(Clone)]
-/// Node for reverse mode
-pub struct Var {
+impl Tape {
+    /// Create a new tape
+    pub fn new() -> Tape {
+        Tape {
+            vars: RefCell::new(Vec::<Node>::new())
+        }
+    }
+
+    /// Add a new (input) variable on the tape
+    pub fn var<'t>(&'t self, value: f64) -> Var<'t> {
+        let len = self.vars.borrow().len();
+        self.vars.borrow_mut().push(
+            Node {
+                partials: [0.0, 0.0],
+                // for a single (input) variable, we point the parents to itself
+                parents: [len, len],
+            }
+        );
+        Var {
+            tape: self,
+            index: len,
+            v: value,
+        }
+    }
+}
+
+/// Variable for computations
+#[derive(Clone, Copy)]
+pub struct Var<'t> {
+    /// Pointer to the tape holding the corresponding node
+    pub tape: &'t Tape,
+    /// Index of the node in the tape
+    pub index: usize,
     /// Value
     pub v: f64,
-    /// Adjoint of this Var
-    pub dv: f64,
-    /// References to the parent nodes, with a weight (partial) value
-    pub parents: Vec<(f64, VarRef)>,
 }
 
-impl Debug for Var {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "v: {}, dv: {:?}, parents: {:?}", self.v, self.dv, self.parents)
-    }
-}
-
-impl Add for Var {
-    type Output = Var;
+impl<'t> Add for Var<'t> {
+    type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let new_v = self.v + rhs.v;
-        let self_rc = Rc::new(RefCell::new(self));
-        let rhs_rc = Rc::new(RefCell::new(rhs));
-
-        Var {
-            v: new_v,
-            dv: 0.0,
-            parents: vec![(1.0, self_rc), (1.0, rhs_rc)],
-        }
-    }
-}
-
-impl Var {
-    /// Create a new node
-    pub fn new(v: f64) -> Var {
-        Var {
-            v,
-            dv: 0.0,
-            parents: Vec::<(f64, VarRef)>::new(),
-        }
-    }
-
-    /// Backprop
-    pub fn backprop(&mut self, adjoint: f64) {
-        // backprop
-        self.dv += adjoint;
-        for parent in self.parents.iter() {
-            println!("parent: {:?}", &parent);
-            (*parent.1).borrow_mut().backprop(parent.0 * adjoint)
-        }
-    }
-
-    /// Call this to get gradients
-    pub fn grad(&mut self) {
-        self.backprop(1.0);
+        // add values
+        // add entry (of the new variable) on tape
+        todo!()
     }
 }
 
@@ -74,10 +71,11 @@ mod tests {
 
     #[test]
     fn addition() {
-        let x: Var = Var::new(1.0);
-        let y: Var = Var::new(2.0);
-        let mut z = x + y;
-        z.grad();
-        println!("z value: {:?}", &z);
+        todo!()
+        // building the computational graph
+
+        // back prop
+
+        // get gradients
     }
 }
