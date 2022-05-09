60b9061deb911c3180fb0d991bf8274f60b648aa
Jingnan Shi <jnshi@mit.edu>
2022-05-09 11:10:18 -0400

    add demo for reverse mode

--- src/bin.rs
+++ src/bin.rs
@@ -1,9 +1,9 @@
 use nalgebra::SMatrix;
 use diff_lib::forward as fdiff;
-
+use diff_lib::reverse as rdiff;
 
 pub fn main() {
-    // API
+    // API for forward mode
     //
     // derivative(f, x)
     let f1_test = |x: fdiff::DualScalar| x * x;
@@ -28,4 +28,13 @@ pub fn main() {
     };
     let f3_result: SMatrix<f64, 2, 2> = fdiff::jacobian(f3_test, vec![1., 2.].as_slice());
     println!("Jacobian of f(x,y) = [x^2 * y , x + y] at ({}, {}) is {:?}", 1.0, 2.0, f3_result);
+
+    // API for reverse mode
+    let tape = rdiff::Tape::new();
+    let x = tape.var(1.0);
+    let y = tape.var(1.0);
+    let z = -2.0 * x + x * x * x * y + 2.0 * y;
+    let grad = z.backprop();
+    println!("dz/dx of z = -2x + x^3 * y + 2y at x=1.0, y=1.0 is {}", grad.wrt(x));
+    println!("dz/dy of z = -2x + x^3 * y + 2y at x=1.0, y=1.0 is {}", grad.wrt(y));
 }
